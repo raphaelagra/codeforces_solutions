2a5ed49dd2ac7385f35a7a1e96c8f50899dc0317
raphaelagra <73735768+raphaelagra@users.noreply.github.com>
2020-10-31 05:07:56 -0300

    Create 59A.py

---
+++ 59A.py
@@ -0,0 +1,15 @@
+palavra = input()
+
+maiuscula = 0
+minuscula = 0
+
+for e in palavra:
+    if e.islower():
+        minuscula += 1
+    else:
+        maiuscula += 1
+
+if maiuscula > minuscula:
+    print(palavra.upper())
+elif minuscula >= maiuscula:
+    print(palavra.lower())
